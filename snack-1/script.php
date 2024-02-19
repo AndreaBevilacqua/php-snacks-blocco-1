e084fe1cfd4c22b0b397d6d95584d62fdaf370ba
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-19 16:18:30 +0100

    created array

---
+++ snack-1/script.php
@@ -0,0 +1,23 @@
+<?php
+
+$games = [
+    [
+    'casa' => 'Olimpia Milano',
+     'ospite' => 'Cantù', 
+     'punti_casa' => 55, 
+     'punti_ospite' => 60
+    ],
+
+    ['casa' => 'Roma',
+     'ospite' => 'Firenze',
+      'punti_casa' => 72,
+       'punti_ospite' => 68
+    ],
+
+    ['casa' => 'Napoli',
+     'ospite' => 'Bologna',
+      'punti_casa' => 88,
+      'punti_ospite' => 78
+    ],
+]
+    
